2b46e5b104cd50ca20975283aeba24c86240ab73
Noah Reece <noah.w.reece@gmail.com>
2023-08-25 16:25:11 -0400

    supply metrics with window dimensions

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -2,11 +2,14 @@ import { Dimensions } from "react-native";
 
 // get width and height of device
 const { width, height } = Dimensions.get("screen");
+const { width: wWidth, height: wHeight } = Dimensions.get("window");
 
 // set screen width and screen height based on detected orientation (portrait/landscape)
 const metrics = {
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
+  windowWidth: wWidth < wHeight ? wWidth : wHeight,
+  windowHeight: wWidth < wHeight ? wHeight : wWidth,
 };
 
 export default metrics;
