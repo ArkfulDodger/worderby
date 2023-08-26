4418291a6837f3013799e96ed94fc79beca52e20
Noah Reece <noah.w.reece@gmail.com>
2023-08-26 15:05:18 -0400

    set input attributes and created flickerless validation

--- src/features/game/components/GameTextInput/GameTextInput.styles.ts
+++ src/features/game/components/GameTextInput/GameTextInput.styles.ts
@@ -2,29 +2,38 @@ import { StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { AppTheme } from "../../../../utils/types";
 
 interface StaticStyles {
-  input: TextStyle;
-  invisible: TextStyle;
+  text: TextStyle;
+  hiddenInput: TextStyle;
 }
 
 interface DynamicStyles {
   underline: (offset: number) => ViewStyle;
+  caret: (height?: number) => ViewStyle;
+  mockInputContainer: (height?: number) => ViewStyle;
 }
 
 export interface Styles extends StaticStyles, DynamicStyles {}
 
 export const createStyles = (theme: AppTheme, fontSize: number): Styles => {
   const staticStyles = StyleSheet.create<StaticStyles>({
-    input: {
+    text: {
       fontSize: fontSize,
-      minWidth: fontSize * 0.6,
+      marginHorizontal: 0,
+      paddingHorizontal: 0,
+      // minWidth: fontSize * 0.6,
     },
-    invisible: {
+    hiddenInput: {
       position: "absolute",
-      opacity: 0,
+      color: "transparent",
     },
   });
 
   const dynamicStyles: DynamicStyles = {
+    mockInputContainer: (height) => ({
+      minWidth: fontSize * 0.6,
+      height: height,
+      flexDirection: "row",
+    }),
     underline: (offset) => ({
       zIndex: -1,
       position: "absolute",
@@ -33,6 +42,13 @@ export const createStyles = (theme: AppTheme, fontSize: number): Styles => {
       left: 0,
       right: 0,
       bottom: offset - fontSize * 0.1,
+    }),
+    caret: (height) => ({
+      position: "absolute",
+      height: height,
+      opacity: height ? 1 : 0,
+      borderRightColor: theme.colors.outline,
+      borderRightWidth: 2,
     }),
   };
 
--- src/features/game/components/GameTextInput/GameTextInput.tsx
+++ src/features/game/components/GameTextInput/GameTextInput.tsx
@@ -1,7 +1,10 @@
 import {
+  NativeSyntheticEvent,
   Platform,
+  Text,
   TextInput,
   TextInputProps,
+  TextInputSelectionChangeEventData,
   View,
   ViewStyle,
 } from "react-native";
@@ -11,7 +14,7 @@ import { useMemo, useState } from "react";
 
 export type Props = TextInputProps & {
   containerStyle?: ViewStyle;
-  inputRef?: React.LegacyRef<TextInput>;
+  inputRef: React.RefObject<TextInput>;
   fontSize?: number;
 };
 
@@ -20,12 +23,33 @@ const GameTextInput = ({
   inputRef,
   fontSize = 30,
   style,
+  value,
+  onChangeText,
   ...props
 }: Props) => {
   const styles = useStyles(createStyles, fontSize, [fontSize]);
 
   // the height of the text, including padding
   const [textHeight, setTextHeight] = useState<number>();
+
+  // whether to show the caret
+  const [caretIndex, setCaretIndex] = useState(0);
+  const [isCaretVisible, setIsCaretVisible] = useState(true);
+  const showCaret = () => setIsCaretVisible(true);
+  const hideCaret = () => setIsCaretVisible(false);
+  const handleSelectionChange = (
+    e: NativeSyntheticEvent<TextInputSelectionChangeEventData>
+  ) => {
+    let isCaretSinglePosition =
+      e.nativeEvent.selection.start === e.nativeEvent.selection.end;
+
+    if (isCaretSinglePosition) {
+      !isCaretVisible && showCaret();
+      setCaretIndex(e.nativeEvent.selection.start);
+    } else if (!isCaretSinglePosition && isCaretVisible) {
+      hideCaret();
+    }
+  };
 
   // the descent of the font (half the distance for android)
   const descent = useMemo(() => {
@@ -41,15 +65,87 @@ const GameTextInput = ({
     return undefined;
   }, [textHeight, fontSize]);
 
+  const caret = isCaretVisible && (
+    <View>
+      <View style={styles.caret(textHeight)} />
+    </View>
+  );
+
+  // notes for caret animation
+
+  // useEffect(() => {
+  //   let caretTimeout: NodeJS.Timeout;
+
+  //   if (children && scrollIndex >= children.length) {
+  //     setIsCaretOn(false);
+  //   } else {
+  //     caretTimeout = setTimeout(() => {
+  //       setIsCaretOn((prev) => !prev);
+  //     }, 700);
+  //   }
+
+  //   return () => {
+  //     clearTimeout(caretTimeout);
+  //   };
+  // }, [isCaretOn, scrollIndex]);
+
+  // const caret = (
+  //   <Animated.View
+  //     entering={FadeIn}
+  //     exiting={FadeOut.duration(600)}
+  //     style={styles.caretContainer}
+  //   >
+  //     <Text style={styles.caret}>{"   "}</Text>
+  //   </Animated.View>
+  // );
+
+  // ensures only English alphabet characters (lower case) are submitted
+  const handleChangeText = (text: string) => {
+    let validatedText = text.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+    if (onChangeText) {
+      return onChangeText(validatedText);
+    } else {
+      return validatedText;
+    }
+  };
+
   return (
     <View style={containerStyle}>
       {descent && <View style={styles.underline(descent)} />}
+      <View style={styles.mockInputContainer(textHeight)}>
+        <Text selectable={false} style={[styles.text, style]}>
+          {value?.slice(0, caretIndex) || ""}
+        </Text>
+        {caret}
+        <Text selectable={false} style={[styles.text, style]}>
+          {value?.slice(caretIndex) || ""}
+        </Text>
+      </View>
       <TextInput
         ref={inputRef}
-        onLayout={(e) => setTextHeight(e.nativeEvent.layout.height)}
-        style={[styles.input, style]}
-        returnKeyLabel="Submit"
+        value={value}
+        style={[styles.text, style, styles.hiddenInput]}
         autoCapitalize="none"
+        autoComplete="off"
+        autoCorrect={false}
+        autoFocus
+        blurOnSubmit={false}
+        caretHidden={true}
+        contextMenuHidden={true}
+        cursorColor={"transparent"}
+        disableFullscreenUI // android
+        enablesReturnKeyAutomatically // ios
+        importantForAutofill="no" // android
+        keyboardType="ascii-capable" // ios
+        maxLength={5}
+        onBlur={hideCaret}
+        onChangeText={handleChangeText}
+        onFocus={showCaret}
+        onLayout={(e) => setTextHeight(e.nativeEvent.layout.height)}
+        onSelectionChange={handleSelectionChange}
+        returnKeyType="go"
+        selectTextOnFocus={false}
         {...props}
       />
     </View>
