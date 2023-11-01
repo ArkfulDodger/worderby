c2754cb354d9439b7a09870827f02fa48156ebf2
Noah Reece <noah.w.reece@gmail.com>
2023-11-01 18:14:02 -0400

    turn counter highlights active turn

--- src/features/game/components/TurnCounter/TurnCounter.styles.ts
+++ src/features/game/components/TurnCounter/TurnCounter.styles.ts
@@ -1,25 +1,35 @@
 import { StyleSheet, ViewStyle } from "react-native";
 import { AppTheme } from "../../../../utils/types";
 
-export interface Styles {
+interface StaticStyles {
   container: ViewStyle;
-  dot: ViewStyle;
 }
+
+interface DynamicStyles {
+  dot: (current?: boolean) => ViewStyle;
+}
+
+export interface Styles extends StaticStyles, DynamicStyles {}
 
 export const createStyles = (theme: AppTheme): Styles => {
   const size = 8;
 
-  return StyleSheet.create<Styles>({
+  const staticStyles = StyleSheet.create<StaticStyles>({
     container: {
       flexDirection: "column-reverse",
       rowGap: 3,
       width: size,
     },
-    dot: {
-      backgroundColor: "#00000088",
+  });
+
+  const dynamicStyles: DynamicStyles = {
+    dot: (current) => ({
+      backgroundColor: current ? "red" : "#00000088",
       width: size,
       height: size,
       borderRadius: size * 0.5,
-    },
-  });
+    }),
+  };
+
+  return { ...staticStyles, ...dynamicStyles };
 };
--- src/features/game/components/TurnCounter/TurnCounter.tsx
+++ src/features/game/components/TurnCounter/TurnCounter.tsx
@@ -13,22 +13,28 @@ const TurnCounter = ({ isPlayer }: Props) => {
   const styles = useStyles(createStyles);
   const { turns, isPlayerFirst } = useAppSelector((state) => state.game);
 
-  // the current active turn number
-  const turnNumber = Math.min(TURNS_PER_GAME, turns.length + 1);
+  // determine if it is this player's turn
+  const isActivePlayer =
+    turns.length < TURNS_PER_GAME && // if there are turns remaining
+    (turns.length % 2 === 0) === (isPlayerFirst === !!isPlayer); // match odd/even turns to player order
 
-  // the given player's turn count
-  const count =
-    (isPlayerFirst && isPlayer) || (!isPlayerFirst && !isPlayer)
-      ? Math.floor(turnNumber * 0.5) + 1
-      : Math.floor(turnNumber * 0.5);
+  // dot renderer
+  const dot = (i: number, active?: boolean) => (
+    <View key={i} style={styles.dot(active)} />
+  );
 
-  // to render a single dot
-  const dot = (i: number) => <View key={i} style={styles.dot} />;
-
-  // the dots array
+  // render regular dots for this player's completed turns
   let dots: JSX.Element[] = [];
-  for (let i = 0; i < count; i++) {
-    dots.push(dot(i));
+  for (let i = 0; i < turns.length; i++) {
+    const turn = turns[i];
+    if (turn.isPlayerWord === !!isPlayer) {
+      dots.push(dot(i));
+    }
+  }
+
+  // render the active dot for the active player
+  if (isActivePlayer) {
+    dots.push(dot(turns.length, true));
   }
 
   return <View style={styles.container}>{dots}</View>;
