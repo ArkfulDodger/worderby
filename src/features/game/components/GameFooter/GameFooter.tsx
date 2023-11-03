9dc37761b4721559fad2d1e6377b51773558b496
Noah Reece <noah.w.reece@gmail.com>
2023-11-03 12:34:22 -0400

    added mute state functionality to footer and placeholders for other footer buttons

--- src/features/game/components/GameFooter/GameFooter.tsx
+++ src/features/game/components/GameFooter/GameFooter.tsx
@@ -3,26 +3,35 @@ import useStyles from "../../../../hooks/useStyles";
 import { createStyles } from "./GameFooter.styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "../../../../components/atoms/Button";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
+import { toggleIsMuted } from "../../../../reducers/systemReducer";
 
 export type Props = {};
 
 // the footer for the game cycle
 const GameFooter = ({}: Props) => {
   const styles = useStyles(createStyles);
+  const dispatch = useAppDispatch();
+  const isMuted = useAppSelector((state) => state.system.isMuted);
 
-  // TODO: use actual state-stored settings
-  const isMuted = false;
+  // toggle mute on mute press
+  const onMutePress = () => dispatch(toggleIsMuted());
 
   // TODO: submit button logic
+  const onSubmitPress = () => console.log("submit pressed");
 
   // TODO: home button logic
+  const onHomePress = () => console.log("home pressed");
 
   return (
     <Surface style={styles.footerContainer}>
       <SafeAreaView edges={["bottom"]} style={styles.footerContent}>
-        <IconButton icon="home" />
-        <Button>SUBMIT</Button>
-        <IconButton icon={isMuted ? "volume-off" : "volume-high"} />
+        <IconButton onPress={onHomePress} icon="home" />
+        <Button onPress={onSubmitPress}>SUBMIT</Button>
+        <IconButton
+          onPress={onMutePress}
+          icon={isMuted ? "volume-off" : "volume-high"}
+        />
       </SafeAreaView>
     </Surface>
   );
