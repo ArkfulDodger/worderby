de6d8caadb8dd341c5464a966769e0a1ac588835
Noah Reece <noah.w.reece@gmail.com>
2023-11-03 12:12:15 -0400

    refactored theme control and reducer

--- src/hooks/useThemeControl.tsx
+++ src/hooks/useThemeControl.tsx
@@ -1,43 +1,19 @@
-import { Appearance, ColorSchemeName } from "react-native";
 import { useAppDispatch, useAppSelector } from "./reduxHooks";
-import {
-  setDarkModeOff,
-  setDarkModeOn,
-  setSystemTheme,
-} from "../reducers/themeReducer";
+import { refreshSystemTheme } from "../reducers/themeReducer";
 import { useEffect, useMemo } from "react";
 import { darkTheme, lightTheme } from "../theme/themes";
 
+// updates system theme on app focus
+// returns active theme object based on current dark mode state
 const useThemeControl = () => {
-  const { systemTheme, themePreference, isDark } = useAppSelector(
-    (state) => state.theme
-  );
+  const { isDark } = useAppSelector((state) => state.theme);
   const { isAppActive } = useAppSelector((state) => state.system);
   const dispatch = useAppDispatch();
 
-  // refresh system theme in state
-  const refreshSystemTheme = () => {
-    const currentSystemTheme = Appearance.getColorScheme(); // will be "dark" or "light"
-    if (systemTheme !== currentSystemTheme)
-      dispatch(setSystemTheme(currentSystemTheme));
-  };
-
-  // set the active theme according the passed Color Scheme
-  const setActiveThemeAs = (colorScheme: ColorSchemeName) => {
-    if (colorScheme === "dark") dispatch(setDarkModeOn());
-    else if (colorScheme === "light") dispatch(setDarkModeOff());
-  };
-
   // when the app regains focus, refresh the system theme in case it has changed
   useEffect(() => {
-    if (isAppActive) refreshSystemTheme();
+    if (isAppActive) dispatch(refreshSystemTheme());
   }, [isAppActive]);
-
-  // update active theme when either system theme or theme preference changes
-  useEffect(() => {
-    if (!themePreference) setActiveThemeAs(systemTheme);
-    else setActiveThemeAs(themePreference);
-  }, [systemTheme, themePreference]);
 
   // memoize the theme object, updating when theme state changes
   const theme = useMemo(() => (isDark ? darkTheme : lightTheme), [isDark]);
--- src/reducers/themeReducer.ts
+++ src/reducers/themeReducer.ts
@@ -1,52 +1,65 @@
 // Import the createSlice API from Redux Toolkit
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { ColorSchemeName } from "react-native";
+import { Appearance, ColorSchemeName } from "react-native";
 
 // Define a type for the slice state
 interface ThemeState {
   isDark: boolean;
   systemTheme: ColorSchemeName;
-  themePreference?: ColorSchemeName;
+  themePreference: ColorSchemeName | "system";
 }
 
 // This is the initial state of the slice
 const initialState: ThemeState = {
   isDark: false,
   systemTheme: "light", // alternative is "dark"
-  // themePreference: undefined
+  themePreference: "system",
 };
 
 export const themeSlice = createSlice({
   name: "theme",
   initialState,
   reducers: {
-    setDarkModeOn: (state) => {
-      state.isDark = true;
-    },
-    setDarkModeOff: (state) => {
-      state.isDark = false;
-    },
-    setSystemTheme: (state, action: PayloadAction<ColorSchemeName>) => {
-      state.systemTheme = action.payload;
+    // refreshes the system theme, updating app theme if needed
+    refreshSystemTheme: (state) => {
+      // get current system theme
+      const systemTheme = Appearance.getColorScheme();
+
+      // update system theme in state
+      state.systemTheme = systemTheme;
+
+      // if app theme preference is "system", update dark mode status
+      if (state.themePreference === "system") {
+        state.isDark = systemTheme === "dark";
+      }
     },
 
-    // update the user's theme preference to the passed color scheme
+    // update the user's theme preference to the passed color scheme, and make active theme
     setThemePreference: (
       state,
-      action: PayloadAction<ColorSchemeName | undefined>
+      action: PayloadAction<ColorSchemeName | "system">
     ) => {
-      state.themePreference = action.payload || undefined;
+      // update the theme preference in state
+      state.themePreference = action.payload;
+
+      // enforce the theme according to the preference
+      switch (action.payload) {
+        case "dark":
+          state.isDark = true;
+          break;
+        case "light":
+          state.isDark = false;
+          break;
+        default:
+          state.isDark = state.systemTheme === "dark";
+          break;
+      }
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {
-  setSystemTheme,
-  setDarkModeOn,
-  setDarkModeOff,
-  setThemePreference,
-} = themeSlice.actions;
+export const { refreshSystemTheme, setThemePreference } = themeSlice.actions;
 
 // We export the reducer function so that it can be added to the store
 export default themeSlice.reducer;
