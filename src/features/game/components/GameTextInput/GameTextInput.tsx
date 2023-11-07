88cb1e6a6a6fa764e31aef7613ec5c0c5796f643
Noah Reece <noah.w.reece@gmail.com>
2023-11-06 19:58:35 -0500

    place input focus logic in GameTextInput. Implement automerge and blurondismiss hooks

--- src/features/game/components/GameTextInput/GameTextInput.tsx
+++ src/features/game/components/GameTextInput/GameTextInput.tsx
@@ -11,32 +11,61 @@ import {
 } from "react-native";
 import useStyles from "../../../../hooks/useStyles";
 import { createStyles } from "./GameTextInput.styles";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useTheme } from "react-native-paper";
 import { AppTheme } from "../../../../utils/types";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
+import useAutoMerge from "../../hooks/useAutoMerge";
+import { setInputFocus, setWordInput } from "../../../../reducers/gameReducer";
+import { selectInputFocus } from "../../gameSelectors";
+import useBlurOnKeyboardDismiss from "../../hooks/useBlurOnKeyboardDismiss";
 
 export type Props = TextInputProps & {
   containerStyle?: ViewStyle;
-  inputRef: React.RefObject<TextInput>;
-  multilineInputRef: React.RefObject<TextInput>;
   fontSize?: number;
 };
 
 // The text input component used during gameplay
 const GameTextInput = ({
   containerStyle,
-  inputRef,
-  multilineInputRef,
   fontSize = 30,
   style,
   value,
   multiline,
-  onChangeText,
   ...props
 }: Props) => {
+  const dispatch = useAppDispatch();
+  const { checkInputForAutoMerge } = useAutoMerge();
+  useBlurOnKeyboardDismiss();
+
   const styles = useStyles(createStyles, fontSize, [fontSize]);
   const { colors } = useTheme() as AppTheme;
   const scaleFactor = PixelRatio.getFontScale();
+  const inputFocused = useAppSelector(selectInputFocus);
+
+  // refs
+  const inputRef = useRef<TextInput>(null);
+  const multilineInputRef = useRef<TextInput>(null);
+
+  // focus or blur the player text input
+  const focusInput = () =>
+    multiline && multilineInputRef.current
+      ? multilineInputRef.current.focus()
+      : inputRef.current?.focus();
+  const blurInput = () =>
+    multiline && multilineInputRef.current
+      ? multilineInputRef.current.blur()
+      : inputRef.current?.blur();
+
+  useEffect(() => {
+    // select the input ref currently in use based on whether the word is split
+    const activeInputRef = multiline ? multilineInputRef : inputRef;
+
+    // focus or blur the input to match the current state
+    if (!inputFocused && activeInputRef.current?.isFocused()) blurInput();
+    else if (inputFocused && !activeInputRef.current?.isFocused()) focusInput();
+    0;
+  }, [inputFocused]);
 
   // the height of the text, including padding
   const [textHeight, setTextHeight] = useState<number>();
@@ -60,6 +89,16 @@ const GameTextInput = ({
     }
   };
 
+  const onFocus = () => {
+    showCaret();
+    if (!inputFocused) dispatch(setInputFocus(true));
+  };
+
+  const onBlur = () => {
+    hideCaret();
+    if (inputFocused) dispatch(setInputFocus(false));
+  };
+
   // focus input on android after first render (keyboard layout bug if immediate focus)
   const [hasRendered, setHasRendered] = useState(false);
   const onComponentRender = () => {
@@ -73,7 +112,8 @@ const GameTextInput = ({
     if (inputRef.current && hasRendered && textHeight && !canFocus) {
       setCanFocus(true);
       setTimeout(() => {
-        inputRef.current?.focus();
+        dispatch(setInputFocus(true));
+        // inputRef.current?.focus();
       }, 1);
     }
   }, [inputRef.current, hasRendered, textHeight]);
@@ -81,8 +121,9 @@ const GameTextInput = ({
   // when multiline or focus settings change, focus the appropriate input
   useEffect(() => {
     if (canFocus) {
-      if (multiline) multilineInputRef.current?.focus();
-      else inputRef.current?.focus();
+      dispatch(setInputFocus(true));
+      // if (multiline) multilineInputRef.current?.focus();
+      // else inputRef.current?.focus();
     }
   }, [multiline]);
 
@@ -100,29 +141,30 @@ const GameTextInput = ({
     return undefined;
   }, [textHeight, fontSize]);
 
+  // the caret to display on the text input
   const caret = isCaretVisible && (
     <View>
       <View style={styles.caret(textHeight)} />
     </View>
   );
 
-  // ensures only English alphabet characters (lower case) are submitted
   const handleChangeText = (text: string) => {
+    // ensure only English alphabet characters (lower case) are submitted
     let validatedText = text.replace(/[^a-zA-Z]/g, "").toLowerCase();
 
-    if (onChangeText) {
-      return onChangeText(validatedText);
-    } else {
-      return validatedText;
-    }
+    // initiate checks for whole word entry and potential auto-merges
+    checkInputForAutoMerge(validatedText);
+
+    // update word input in state
+    dispatch(setWordInput(validatedText));
   };
 
   // the base props utilized by the text input
   const inputBaseProps: Partial<TextInputProps> = {
     value: value,
-    onBlur: hideCaret,
+    onBlur: onBlur,
     onChangeText: handleChangeText,
-    onFocus: showCaret,
+    onFocus: onFocus,
     autoFocus: false,
     autoCapitalize: "none",
     autoComplete: "off",
