218c05f09550ec25683eda2c598acee6f04996ad
Noah Reece <noah.w.reece@gmail.com>
2023-11-06 19:54:13 -0500

    made useBlurOnKeyboardDismiss for game input field

---
+++ src/features/game/hooks/useBlurOnKeyboardDismiss.ts
@@ -0,0 +1,34 @@
+import { Keyboard } from "react-native";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
+import { setInputFocus } from "../../../reducers/gameReducer";
+import { selectInputFocus } from "../gameSelectors";
+import { useEffect } from "react";
+
+// ensures the game text input is blurred when the keyboard is dismissed
+const useBlurOnKeyboardDismiss = () => {
+  const inputFocused = useAppSelector(selectInputFocus);
+  const dispatch = useAppDispatch();
+
+  // blur text input if currently focused
+  const blurTextInput = () => {
+    if (inputFocused) dispatch(setInputFocus(false));
+  };
+
+  // Add event listener for the keyboard
+  useEffect(() => {
+    // Subscribe to the keyboard dismiss event
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      blurTextInput
+    );
+
+    return () => {
+      // Remove the event listener when the component unmounts
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  return;
+};
+
+export default useBlurOnKeyboardDismiss;
