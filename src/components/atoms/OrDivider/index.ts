9ff54ea0fafcbdc9e442439ff4a0b458a82a8fc4
Noah Reece <noah.w.reece@gmail.com>
2023-11-10 14:40:10 -0500

    create basic OrDivider and Modal components

---
+++ src/components/atoms/OrDivider/index.ts
@@ -0,0 +1,8 @@
+import OrDivider from "./OrDivider";
+export default OrDivider;
+
+// export { Props as OrDividerProps } from "./OrDivider";
+// export {
+//  Styles as OrDividerStyles,
+//  createStyles as createOrDividerStyles,
+//} from "./OrDivider.styles";
