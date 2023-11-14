a922d8f5f4228133ba0ae1f60d0866b572bb7d5b
Noah Reece <noah.w.reece@gmail.com>
2023-11-14 17:15:43 -0500

    android circle animation fix

--- src/components/molecules/CircularProgressBar/CircularProgressBar.tsx
+++ src/components/molecules/CircularProgressBar/CircularProgressBar.tsx
@@ -3,10 +3,14 @@ import useStyles from "../../../hooks/useStyles";
 import { createStyles } from "./CircularProgressBar.styles";
 import Animated, {
   SharedValue,
+  createAnimatedPropAdapter,
   interpolateColor,
+  processColor,
   useAnimatedProps,
   useDerivedValue,
 } from "react-native-reanimated";
+import { ColorValue } from "react-native";
+import { TIMER_COUNT } from "../../../features/game/constants";
 
 type Props = {
   radius: number;
@@ -19,38 +23,82 @@ const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 const CircularProgressBar = ({ radius, progress, timer }: Props) => {
   const styles = useStyles(createStyles);
 
+  const negativeColor = "#e61934";
+
   const primaryColor = "#1f92f0";
   const fadedPrimary = "#98cbf5";
-  const negativeColor = "#e61934";
   const fadedNegative = "#e37685";
-
-  const R = radius - 3;
-  const circumference = 2 * Math.PI * R;
 
   const activePos = useDerivedValue(() => timer.value > -1);
   const activeBack = useDerivedValue(() => timer.value > -2);
 
-  const underAnimatedProps = useAnimatedProps<CircleProps>(() => ({
-    opacity: progress.value,
-    stroke: interpolateColor(
-      1,
-      [0, 1],
-      activeBack.value
-        ? [fadedPrimary, fadedPrimary]
-        : [fadedNegative, fadedNegative]
-    ),
-  }));
+  const R = radius - 3;
+  const circumference = 2 * Math.PI * R;
 
-  const animatedProps = useAnimatedProps<CircleProps>(() => ({
-    strokeDashoffset: circumference * progress.value,
-    stroke: interpolateColor(
+  // const backstrokeColor = useDerivedValue<ColorValue | undefined>(() =>
+  //   interpolateColor(
+  //     1,
+  //     [0, 1],
+  //     activeBack.value
+  //       ? [fadedPrimary, fadedPrimary]
+  //       : [fadedNegative, fadedNegative]
+  //   )
+  // );
+
+  const backstrokeColor = useDerivedValue<ColorValue | undefined>(() =>
+    timer.value < TIMER_COUNT - 1
+      ? activeBack.value
+        ? fadedPrimary
+        : fadedNegative
+      : "transparent"
+  );
+
+  const strokeColor = useDerivedValue<ColorValue | undefined>(() =>
+    interpolateColor(
       progress.value,
       [0, 1],
       activePos.value
         ? [fadedPrimary, primaryColor]
         : [fadedNegative, negativeColor]
-    ),
-  }));
+    )
+  );
+
+  const adapter = createAnimatedPropAdapter(
+    (props) => {
+      if (Object.keys(props).includes("fill")) {
+        props.fill = {
+          type: 0,
+          payload: processColor(props.fill as string | number),
+        };
+      }
+      if (Object.keys(props).includes("stroke")) {
+        props.stroke = {
+          type: 0,
+          payload: processColor(props.stroke as string | number),
+        };
+      }
+    },
+    ["fill", "stroke"]
+  );
+
+  const underAnimatedProps = useAnimatedProps<CircleProps>(
+    () => ({
+      opacity: progress.value,
+      stroke: backstrokeColor.value,
+      // stroke: backstrokeColor.value,
+    }),
+    [],
+    adapter
+  );
+
+  const animatedProps = useAnimatedProps<CircleProps>(
+    () => ({
+      strokeDashoffset: circumference * progress.value,
+      stroke: strokeColor.value,
+    }),
+    [],
+    adapter
+  );
 
   return (
     <Svg height={radius * 2} width={radius * 2}>
