c3511b1d949d9b58f00ebab86d0e77d9ee242e89
Noah Reece <noah.w.reece@gmail.com>
2023-11-15 12:32:58 -0500

    refactored circular progress bar

--- src/components/molecules/CircularProgressBar/CircularProgressBar.tsx
+++ src/components/molecules/CircularProgressBar/CircularProgressBar.tsx
@@ -1,6 +1,4 @@
 import { Circle, CircleProps, G, Svg } from "react-native-svg";
-import useStyles from "../../../hooks/useStyles";
-import { createStyles } from "./CircularProgressBar.styles";
 import Animated, {
   SharedValue,
   createAnimatedPropAdapter,
@@ -18,51 +16,54 @@ type Props = {
   timer: SharedValue<number>;
 };
 
+// Create an animated version of the Circle Component
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
+// The colors for the progress timer
+const POSITIVE_COLOR = "#1f92f0";
+const POSITIVE_COLOR_FADED = "#98cbf5";
+const NEGATIVE_COLOR = "#e61934";
+const NEGATIVE_COLOR_FADED = "#e37685";
+
+// a circular progress bar which loops over itself over the course of a timer countdown
+// Renders an actively progressing circle svg above a backdrop circle to "fade out" the previous
+// progress bar will change color when progressing to negative values
 const CircularProgressBar = ({ radius, progress, timer }: Props) => {
-  const styles = useStyles(createStyles);
+  // the width of the progress bar line
+  const strokeWidth = 6;
 
-  const negativeColor = "#e61934";
+  // the effective radius (center to center of stroke)
+  const R = radius - strokeWidth * 0.5;
 
-  const primaryColor = "#1f92f0";
-  const fadedPrimary = "#98cbf5";
-  const fadedNegative = "#e37685";
-
-  const activePos = useDerivedValue(() => timer.value > -1);
-  const activeBack = useDerivedValue(() => timer.value > -2);
-
-  const R = radius - 3;
+  // the effective circumference (along center of stroke)
   const circumference = 2 * Math.PI * R;
 
-  // const backstrokeColor = useDerivedValue<ColorValue | undefined>(() =>
-  //   interpolateColor(
-  //     1,
-  //     [0, 1],
-  //     activeBack.value
-  //       ? [fadedPrimary, fadedPrimary]
-  //       : [fadedNegative, fadedNegative]
-  //   )
-  // );
+  // which color (positive/negative) is in place for the given circle
+  const isActiveCirclePositive = useDerivedValue(() => timer.value > -1);
+  const isBackCirclePositive = useDerivedValue(() => timer.value > -2);
 
-  const backstrokeColor = useDerivedValue<ColorValue | undefined>(() =>
-    timer.value < TIMER_COUNT - 1
-      ? activeBack.value
-        ? fadedPrimary
-        : fadedNegative
-      : "transparent"
-  );
-
+  // the color of the active stroke, which "fades" over each progress interval
   const strokeColor = useDerivedValue<ColorValue | undefined>(() =>
     interpolateColor(
       progress.value,
       [0, 1],
-      activePos.value
-        ? [fadedPrimary, primaryColor]
-        : [fadedNegative, negativeColor]
+      isActiveCirclePositive.value
+        ? [POSITIVE_COLOR_FADED, POSITIVE_COLOR]
+        : [NEGATIVE_COLOR_FADED, NEGATIVE_COLOR]
     )
   );
 
+  // the color of the background stroke, which starts on the second cound to "fade" the first stroke
+  const backstrokeColor = useDerivedValue<ColorValue | undefined>(() =>
+    timer.value < TIMER_COUNT - 1
+      ? isBackCirclePositive.value
+        ? POSITIVE_COLOR_FADED
+        : NEGATIVE_COLOR_FADED
+      : "transparent"
+  );
+
+  // the adapter function which allows svg & reanimated to properly convert color types
+  // necessary to avoid Android crash
   const adapter = createAnimatedPropAdapter(
     (props) => {
       if (Object.keys(props).includes("fill")) {
@@ -81,20 +82,23 @@ const CircularProgressBar = ({ radius, progress, timer }: Props) => {
     ["fill", "stroke"]
   );
 
-  const underAnimatedProps = useAnimatedProps<CircleProps>(
+  // the animated properties of the active stroke
+  // animates the progress bar filling the circumference over time while fading
+  const activeAnimatedProps = useAnimatedProps<CircleProps>(
     () => ({
-      opacity: progress.value,
-      stroke: backstrokeColor.value,
-      // stroke: backstrokeColor.value,
+      strokeDashoffset: circumference * progress.value,
+      stroke: strokeColor.value,
     }),
     [],
     adapter
   );
 
-  const animatedProps = useAnimatedProps<CircleProps>(
+  // the animated properties of the background stroke
+  // animates the prior active stroke fading to 0 opacity
+  const underAnimatedProps = useAnimatedProps<CircleProps>(
     () => ({
-      strokeDashoffset: circumference * progress.value,
-      stroke: strokeColor.value,
+      opacity: progress.value,
+      stroke: backstrokeColor.value,
     }),
     [],
     adapter
@@ -108,17 +112,17 @@ const CircularProgressBar = ({ radius, progress, timer }: Props) => {
           cx={radius}
           cy={radius}
           r={R}
-          strokeWidth={6}
+          strokeWidth={strokeWidth}
           fill="transparent"
         />
         <AnimatedCircle
-          animatedProps={animatedProps}
+          animatedProps={activeAnimatedProps}
           strokeDasharray={circumference}
           strokeLinecap={"round"}
           cx={radius}
           cy={radius}
           r={R}
-          strokeWidth={6}
+          strokeWidth={strokeWidth}
           fill="transparent"
         />
       </G>
