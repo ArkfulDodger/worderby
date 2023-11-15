29b8ab25eca203b739b5050346a3c840bb396f3d
Noah Reece <noah.w.reece@gmail.com>
2023-11-15 18:39:26 -0500

    restrictions refactoring

--- src/features/game/components/RestrictionTextItem/RestrictionTextItem.tsx
+++ src/features/game/components/RestrictionTextItem/RestrictionTextItem.tsx
@@ -10,6 +10,8 @@ export type RestrictionMatch = {
 
 export type Props = { str: string; matchNum: number };
 
+// the text element for an individual word ending on the restricted list
+// styled according to how it does or doesn't match the current play word input
 const RestrictionTextItem = ({ str, matchNum }: Props) => {
   const styles = useStyles(createStyles);
 
--- src/features/game/components/RestrictionsBlock/RestrictionsBlock.tsx
+++ src/features/game/components/RestrictionsBlock/RestrictionsBlock.tsx
@@ -1,106 +1,31 @@
 import { LayoutChangeEvent, Pressable, View } from "react-native";
 import useStyles from "../../../../hooks/useStyles";
 import { createStyles } from "./RestrictionsBlock.styles";
-import { useAppSelector } from "../../../../hooks/reduxHooks";
-import { selectPlayWord, selectRestrictions } from "../../gameSelectors";
-import { useEffect, useMemo, useState } from "react";
-import useSelfReplacingTimeout from "../../../../hooks/useSelfReplacingTimeout";
-import RestrictionTextItem, {
-  RestrictionTextItemProps,
-} from "../RestrictionTextItem";
+import { useState } from "react";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
 } from "react-native-reanimated";
 import { RESTRICTION_ITEM_HEIGHT } from "../RestrictionTextItem/RestrictionTextItem.styles";
+import useRestrictionLists from "../../hooks/useRestrictionLists";
 
 type Props = {};
 
+// the header area to display the restricted endings. If the player turn,
+// will highlight restrictions which relate to the current input
 const RestrictionsBlock = ({}: Props) => {
   const styles = useStyles(createStyles);
-  const restrictions = useAppSelector(selectRestrictions);
-  const playWord = useAppSelector(selectPlayWord);
-  const setRefreshMatchTimeout = useSelfReplacingTimeout();
 
-  // the array of ending matches
-  const [restritionItems, setRestrictionItems] = useState<
-    RestrictionTextItemProps[]
-  >([]);
+  // get the different lists of restricted endings to display in the component
+  const { blockedEndings, regularEndings, fadedEndings } =
+    useRestrictionLists();
 
-  const getMatches = () => {
-    let matches = restrictions
-      .map<RestrictionTextItemProps>((r) => {
-        if (playWord.endsWith(r))
-          return {
-            str: r,
-            matchNum: Infinity,
-          };
-
-        for (let i = r.length - 1; i > 0; i--) {
-          const substring = r.slice(0, i);
-          if (playWord.endsWith(substring))
-            return {
-              str: r,
-              matchNum: i,
-            };
-        }
-
-        return {
-          str: r,
-          matchNum: 0,
-        };
-      })
-      .sort((a, b) => b.matchNum - a.matchNum);
-
-    return matches;
-  };
-
-  useEffect(() => {
-    if (playWord !== undefined && playWord !== "") {
-      setRefreshMatchTimeout(() => {
-        const refreshedMatches = getMatches();
-        setRestrictionItems(refreshedMatches);
-      }, 300);
-    } else {
-      setRestrictionItems(
-        restrictions.map<RestrictionTextItemProps>((r) => ({
-          str: r,
-          matchNum: 1,
-        }))
-      );
-    }
-  }, [playWord]);
-
-  const blockedEndings = useMemo(
-    () =>
-      restritionItems
-        .filter((item) => item.matchNum === Infinity)
-        .map((props) => <RestrictionTextItem key={props.str} {...props} />),
-    [restritionItems]
-  );
-  const regularEndings = useMemo(
-    () =>
-      restritionItems
-        .filter((item) => item.matchNum < Infinity && item.matchNum > 0)
-        .map((props) => <RestrictionTextItem key={props.str} {...props} />),
-    [restritionItems]
-  );
-  const fadedEndings = useMemo(
-    () =>
-      restritionItems
-        .filter((item) => item.matchNum === 0)
-        .map((props) => <RestrictionTextItem key={props.str} {...props} />),
-    [restritionItems]
-  );
-
+  // animated style control for the restriction component layout
   const containerHeight = useSharedValue(RESTRICTION_ITEM_HEIGHT * 2);
-
   const containerStyles = useAnimatedStyle(() => ({
     height: containerHeight.value,
   }));
-
   const [isCentered, setIsCentered] = useState(false);
-
   const onLayout = (e: LayoutChangeEvent) => {
     if (e.nativeEvent.layout.height >= RESTRICTION_ITEM_HEIGHT * 2) {
       setIsCentered(false);
