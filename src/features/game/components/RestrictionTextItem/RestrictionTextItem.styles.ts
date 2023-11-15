1d992c7162e2341466d268cdab5c6c4ba725f4b2
Noah Reece <noah.w.reece@gmail.com>
2023-11-15 18:11:26 -0500

    enforce restricion text items to fixed height

--- src/features/game/components/RestrictionTextItem/RestrictionTextItem.styles.ts
+++ src/features/game/components/RestrictionTextItem/RestrictionTextItem.styles.ts
@@ -1,14 +1,36 @@
-import { TextStyle } from "react-native";
+import { PixelRatio, StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { AppTheme } from "../../../../utils/types";
 
-export interface Styles {
+interface StaticStyles {
+  container: ViewStyle;
+}
+
+interface DynamicStyles {
   endings: (n: number) => TextStyle;
 }
 
-export const createStyles = (theme: AppTheme): Styles => ({
-  endings: (n) => ({
-    color: n === Infinity ? "red" : theme.colors.text,
-    fontWeight: n > 1 ? "bold" : "normal",
-    opacity: n === 0 ? 0.4 : 1,
-  }),
-});
+const scaleFactor = PixelRatio.getFontScale();
+const FONT_SIZE = 16;
+export const RESTRICTION_ITEM_HEIGHT = scaleFactor * (FONT_SIZE * 1.5);
+
+export interface Styles extends StaticStyles, DynamicStyles {}
+
+export const createStyles = (theme: AppTheme): Styles => {
+  const staticStyles = StyleSheet.create<StaticStyles>({
+    container: {
+      height: RESTRICTION_ITEM_HEIGHT,
+      justifyContent: "center",
+    },
+  });
+
+  const dynamicStyles: DynamicStyles = {
+    endings: (n) => ({
+      fontSize: FONT_SIZE,
+      color: n === Infinity ? "red" : theme.colors.text,
+      fontWeight: n > 1 ? "bold" : "normal",
+      opacity: n === 0 ? 0.4 : 1,
+    }),
+  };
+
+  return { ...staticStyles, ...dynamicStyles };
+};
--- src/features/game/components/RestrictionTextItem/RestrictionTextItem.tsx
+++ src/features/game/components/RestrictionTextItem/RestrictionTextItem.tsx
@@ -1,6 +1,7 @@
 import { Text } from "react-native-paper";
 import useStyles from "../../../../hooks/useStyles";
 import { createStyles } from "./RestrictionTextItem.styles";
+import { View } from "react-native";
 
 export type RestrictionMatch = {
   str: string;
@@ -12,7 +13,11 @@ export type Props = { str: string; matchNum: number };
 const RestrictionTextItem = ({ str, matchNum }: Props) => {
   const styles = useStyles(createStyles);
 
-  return <Text style={styles.endings(matchNum)}>-{str}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.endings(matchNum)}>-{str}</Text>
+    </View>
+  );
 };
 
 export default RestrictionTextItem;
