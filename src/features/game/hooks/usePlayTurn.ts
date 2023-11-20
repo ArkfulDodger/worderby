321a1b30846296a95b9a610df7194d05c5a0e82d
Noah Reece <noah.w.reece@gmail.com>
2023-11-20 10:41:07 -0500

    validate word attempts against restrictions and word list

--- src/features/game/hooks/usePlayTurn.ts
+++ src/features/game/hooks/usePlayTurn.ts
@@ -6,31 +6,31 @@ import { GameMode } from "../enums";
 import {
   selectActiveTurn,
   selectMode,
+  selectRestrictions,
   selectUsedUnusedPrompt,
   selectWordInput,
 } from "../gameSelectors";
+import { errorToast, warnToast } from "../gameToastHelpers";
 
+// logic for letting the user attempt to play a word in the current game
 const usePlayTurn = () => {
   const dispatch = useAppDispatch();
   const { usedPrompt } = useAppSelector(selectUsedUnusedPrompt);
   const wordInput = useAppSelector(selectWordInput);
   const activeTurn = useAppSelector(selectActiveTurn);
   const mode = useAppSelector(selectMode);
+  const restrictions = useAppSelector(selectRestrictions);
   const { isWordOnList } = useWordList();
 
+  // checks for whether there are any errors to prevent the word attempt
   const isAttemptWordError = () => {
-    // confirm usedPrompt and wordInput are usable
+    // confirm usedPrompt is usable
     if (usedPrompt.length < 1) {
       console.error(
         "Attempt Word Error:",
         "Need to select at least one letter from prompt"
       );
-      return true;
-    }
-
-    // confirm usedPrompt and wordInput are usable
-    if (!wordInput || wordInput.length < 1) {
-      console.error("Attempt Word Error:", "Must add at least one letter.");
+      errorToast("error: no prompt letters used");
       return true;
     }
 
@@ -40,54 +40,103 @@ const usePlayTurn = () => {
         "Attempt Word Error:",
         "There is no active turn data to play"
       );
+      errorToast("error: no active turn detected");
       return true;
     }
   };
 
-  const attemptWord = async () => {
-    // note the timestamp
-    const timestamp = new Date().toISOString();
-    const timerCount = activeTurn?.timerCount;
-
-    // start loading
-    dispatch(setIsLoading(true));
-
-    // check errors
-    if (!activeTurn || isAttemptWordError()) {
-      dispatch(setIsLoading(false));
-      return;
-    }
-
-    // get the word
-    const word = usedPrompt + wordInput;
-
-    // TODO: confirm word exists
-    try {
-      const wordExists = await isWordOnList(word);
-      if (!wordExists) {
-        dispatch(setIsLoading(false));
-        return;
-      }
-    } catch (error) {
-      dispatch(setIsLoading(false));
-      return;
-    }
-
-    // calculate final endTimer and startedAt values
+  // based on the played-at timestamp, recorded start time, and last registered timer count,
+  // determine final timer and start time values for a turn
+  const getFinalTimerValues = (
+    playdAtTimestamp: string,
+    startTime: string | undefined,
+    timerCount: number | undefined
+  ) => {
     let endTimer = timerCount;
-    let finalStartTime = activeTurn.startTime;
+    let finalStartTime = startTime;
 
     if (finalStartTime) {
       // if there is a start time, calculate the official end timer
       endTimer =
         mode === GameMode.Casual
           ? undefined
-          : getEndTimer(finalStartTime, timestamp, timerCount);
+          : getEndTimer(finalStartTime, playdAtTimestamp, timerCount);
     } else {
       // if there is no start time, calculate it from the last known timer count
       // will assume the default TIMER_COUNT if no timer count is found/provided, as for casual games
-      finalStartTime = deriveStartTimeFromTimer(timestamp, timerCount);
+      finalStartTime = deriveStartTimeFromTimer(playdAtTimestamp, timerCount);
     }
+
+    return { endTimer, finalStartTime };
+  };
+
+  // return whether a word can be played for the current turn
+  const isWordValid = async (word: string) => {
+    // confirm user has provided input
+    if (!wordInput || wordInput.length < 1) {
+      warnToast("Must add at least one letter!");
+      return false;
+    }
+
+    // confirm does not end in any restricted ending
+    for (let i = 0; i < restrictions.length; i++) {
+      const restriction = restrictions[i];
+      if (word.endsWith(restriction)) {
+        warnToast(`Word cannot end in -${restriction}`);
+        return false;
+      }
+    }
+
+    // confirm is real word (validate from word list)
+    try {
+      const wordExists = await isWordOnList(word);
+      if (!wordExists) {
+        warnToast("Oops! Not a word!");
+        return false;
+      }
+    } catch (error) {
+      errorToast("Error: could not verify if word exists");
+      return false;
+    }
+
+    // TODO: confirm is not excluded language (hate speech, etc.)
+
+    return true;
+  };
+
+  // stops the play attempt, ensuring loading is false so play may continue
+  const stopAttempt = () => {
+    dispatch(setIsLoading(false));
+    return;
+  };
+
+  // called when the user attempts to play a word for their turn
+  // results in either the turn being played, or the word being rejected as unplayable
+  const attemptWord = async () => {
+    // note the time values for when the attempt was made
+    const timestamp = new Date().toISOString();
+    const timerCount = activeTurn?.timerCount;
+
+    // clear any previous toast messages
+    gameToast?.hideAll();
+
+    // start loading to prevent repeat submissions
+    dispatch(setIsLoading(true));
+
+    // check for word attempt errors
+    if (!activeTurn || isAttemptWordError()) return stopAttempt();
+
+    // get and validate the word
+    const word = usedPrompt + wordInput;
+    const isValid = await isWordValid(word);
+    if (!isValid) return stopAttempt();
+
+    // determine final values to use for timer and start time
+    let { endTimer, finalStartTime } = getFinalTimerValues(
+      timestamp,
+      activeTurn.startTime,
+      timerCount
+    );
 
     // make turn object
     const turn: Turn = {
