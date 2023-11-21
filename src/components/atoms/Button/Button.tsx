c3bad2e2dabd2efeee3ace5c605ad77d5e80a7c3
Noah Reece <noah.w.reece@gmail.com>
2023-11-20 21:57:22 -0500

    allow button to be wrapped in router link

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,13 +1,15 @@
+import { forwardRef } from "react";
+import { View } from "react-native";
 import { ButtonProps, Button as PaperButton } from "react-native-paper";
 
-const Button = (props: ButtonProps) => {
+const Button = forwardRef<View, ButtonProps>((props: ButtonProps, ref) => {
   const buttonProps: ButtonProps = {
     mode: "contained",
     uppercase: false,
     ...props,
   };
 
-  return <PaperButton {...buttonProps} />;
-};
+  return <PaperButton {...buttonProps} ref={ref} />;
+});
 
 export default Button;
