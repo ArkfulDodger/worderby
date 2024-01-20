9f27833b6902a709f334c27c7edc1207cb1b0139
Noah Reece <noah.w.reece@gmail.com>
2024-01-19 19:19:57 -0500

    wrapped app in gesture handler

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,19 @@
+import "react-native-gesture-handler";
 import { Slot } from "expo-router";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store/store";
 import AppWrapper from "../features/app-wrapper/AppWrapper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const RootLayout = () => {
   return (
-    <ReduxProvider store={store}>
-      <AppWrapper>
-        <Slot />
-      </AppWrapper>
-    </ReduxProvider>
+    <GestureHandlerRootView>
+      <ReduxProvider store={store}>
+        <AppWrapper>
+          <Slot />
+        </AppWrapper>
+      </ReduxProvider>
+    </GestureHandlerRootView>
   );
 };
 
