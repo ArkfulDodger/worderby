875bfe734d97e3d7ffb4293648512725b9e9b439
Noah Reece <noah.w.reece@gmail.com>
2024-03-20 19:12:25 -0400

    clean up dev code

--- src/features/app-wrapper/Providers/WordListProvider.tsx
+++ src/features/app-wrapper/Providers/WordListProvider.tsx
@@ -5,7 +5,7 @@ import * as FileSystem from "expo-file-system";
 import { Asset } from "expo-asset";
 import { Alert } from "react-native";
 import { useAppDispatch } from "../../../hooks/reduxHooks";
-import { addDevLog, setIsLoadingDb } from "../../../store/slices/systemSlice";
+import { setIsLoadingDb } from "../../../store/slices/systemSlice";
 
 export const WordListContext = createContext<SQLite.SQLiteDatabase | undefined>(
   undefined
@@ -19,9 +19,6 @@ interface Props {
 const WordListProvider = ({ children }: Props) => {
   const dispatch = useAppDispatch();
   const [db, setDb] = useState<SQLite.SQLiteDatabase>();
-  const log = (str: string) => {
-    dispatch(addDevLog(str));
-  };
 
   // loads/creates the database and returns the db object
   async function loadDatabase(): Promise<void> {
@@ -29,44 +26,20 @@ const WordListProvider = ({ children }: Props) => {
     // designate loading the database in state
     dispatch(setIsLoadingDb(true));
 
-    log("Checking in FileSystem if SQLite directory exists...");
     // if there is no SQLite directory on the device, create it
     if (
       !(await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite"))
         .exists
     ) {
-      log("    ...no");
-      log("Making SQLite directory with FileSystem");
       await FileSystem.makeDirectoryAsync(
         FileSystem.documentDirectory + "SQLite"
       );
-    } else {
-      log("    ...yes");
     }
 
     // get the database asset and the location where it needs to be
     const asset = Asset.fromModule(require("../../../assets/data/word.db"));
     let sqlFileUri = FileSystem.documentDirectory + "SQLite/word.db";
-    log(`[asset uri]: ${asset.uri}`);
-    log(`[sqlFileUri]: ${sqlFileUri}`);
 
-    // Alert.alert(
-    //   "asset:",
-    //   `${JSON.stringify(
-    //     {
-    //       name: asset.name,
-    //       type: asset.type,
-    //       uri: asset.uri,
-    //       localUri: asset.localUri,
-    //       downloaded: asset.downloaded,
-    //       downloading: asset.downloading,
-    //     },
-    //     null,
-    //     2
-    //   )}`
-    // );
-
-    log(`Attempting FileSystem download of Db from Expo remote...`);
     // Attempt to Download the Database from the Expo remote asset
     let downloadResult = asset.uri
       ? await FileSystem.downloadAsync(asset.uri, sqlFileUri)
@@ -75,14 +48,8 @@ const WordListProvider = ({ children }: Props) => {
     // If the download fails, download via asset and copy over
     if (!downloadResult || downloadResult.status !== 200) {
       // download asset if not already done
-      log("    ...failed");
-      log("checking if Module asset downloaded...");
       if (!asset.downloaded) {
-        log("    ...no, downloading...");
         await asset.downloadAsync();
-        log("    ...... complete!");
-      } else {
-        log("    ...yes!");
       }
       if (asset.localUri) {
         // copy the database to its proper location if a local database exists
@@ -95,15 +62,10 @@ const WordListProvider = ({ children }: Props) => {
         );
         throw new Error("Word List could not be loaded!");
       }
-    } else {
-      log("    ...success!");
     }
 
-    log("IMPORTANT STEP:");
-    log("Attempting to open the database...");
     // open the database and set the reference in state
     const wordDb = SQLite.openDatabase("word.db");
-    log(`    ...Completed attempt. Name: ${wordDb._name || "[none]"}`);
     setDb(wordDb);
 
     dispatch(setIsLoadingDb(false));
--- src/features/landing/screens/LandingScreen/LandingScreen.tsx
+++ src/features/landing/screens/LandingScreen/LandingScreen.tsx
@@ -5,13 +5,6 @@ import Button from "../../../../components/atoms/Button";
 import useLoadNewGame from "../../../game/hooks/useLoadNewGame";
 import { GameMode } from "../../../game/enums";
 import { useRouter } from "expo-router";
-import { useAppSelector } from "../../../../hooks/reduxHooks";
-import {
-  selectDevLogs,
-  selectIsLoadingDb,
-} from "../../../../store/slices/systemSlice";
-import { ActivityIndicator, Text } from "react-native-paper";
-import { ScrollView } from "react-native-gesture-handler";
 
 type Props = {};
 
@@ -20,8 +13,6 @@ const LandingScreen = ({}: Props) => {
   const styles = useStyles(createStyles);
   const { loadSinglePlayerGame } = useLoadNewGame();
   const router = useRouter();
-  const isDbLoading = useAppSelector(selectIsLoadingDb);
-  const devLogs = useAppSelector(selectDevLogs);
 
   const onDemoPress = () => {
     router.push("/game/demo");
@@ -46,33 +37,6 @@ const LandingScreen = ({}: Props) => {
   return (
     <View style={styles.container}>
       {/* <Button onPress={onDemoPress}>Demo</Button> */}
-      {isDbLoading && (
-        <View style={{ alignItems: "center" }}>
-          <ActivityIndicator />
-          <Text>Database is loading...</Text>
-        </View>
-      )}
-      {true && (
-        <View>
-          <Text>Logs</Text>
-          <View
-            style={{
-              height: 200,
-              borderWidth: 1,
-              padding: 10,
-              borderRadius: 10,
-            }}
-          >
-            <ScrollView contentContainerStyle={{ rowGap: 10 }}>
-              {devLogs.map((log, i) => (
-                <Text numberOfLines={2} key={i.toString()}>
-                  {log}
-                </Text>
-              ))}
-            </ScrollView>
-          </View>
-        </View>
-      )}
       <Button onPress={onCompetitivePress}>Competitive</Button>
       <Button onPress={onCasualPress}>Casual</Button>
     </View>
